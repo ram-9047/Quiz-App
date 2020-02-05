406a96f18e20eb3a5ce0d96979152e437ada8d6b
ram-9047 <ramg786@live.com>
2020-02-05 13:55:05 +0530

    initial commit

--- client/index.js
+++ client/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 import Store from "./store/index";
+
 import "../stylesheets/reset.css";
 
 import Homepage from "./homepage/homepage.js";
--- client/profile/profile.js
+++ client/profile/profile.js
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 class Profile extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
   }
   handleLogOut = () => {
     localStorage.clear();
